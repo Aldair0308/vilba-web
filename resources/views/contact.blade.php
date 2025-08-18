4498a3b82afc7fce23283fd7bfcd690858097acd
Aldair0308 <aldairm0308@gmail.com>
2025-08-18 15:11:56 -0600

    refactor(views): update contact page head section and add language support
    - Replace session-based language with $language variable for consistency
    - Add manifest link and update favicon path
    - Include language switcher script and currentLanguage variable
    - Fix duplicate themify-icons.css import

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,29 +2,35 @@
 <html class="no-js" lang="{{ session('language', 'es') }}">
 
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>{{ session('language') === 'en' ? 'Contact - Vilba Construction' : 'Contacto - Vilba Construcción' }}</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/img/favicon.ico') }}">
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>{{ $language === 'en' ? 'Services - Vilba' : 'Servicios - Vilba' }}</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="manifest" href="site.webmanifest">
+		<link rel="shortcut icon" type="image/x-icon" href="/assets/img/favicon.ico">
 
    <!-- CSS here -->
-        <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('assets/css/owl.carousel.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('assets/css/gijgo.css') }}">
-        <link rel="stylesheet" href="{{ asset('assets/css/slicknav.css') }}">
-        <link rel="stylesheet" href="{{ asset('assets/css/animate.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('assets/css/magnific-popup.css') }}">
-        <link rel="stylesheet" href="{{ asset('assets/css/fontawesome-all.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('assets/css/themify-icons.css') }}">
-        <link rel="stylesheet" href="{{ asset('assets/css/themify-icons.css') }}">
-        <link rel="stylesheet" href="{{ asset('assets/css/slick.css') }}">
-        <link rel="stylesheet" href="{{ asset('assets/css/nice-select.css') }}">
-        <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
-        <link rel="stylesheet" href="{{ asset('assets/css/responsive.css') }}">
-</head>
+        <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/assets/css/owl.carousel.min.css">
+        <link rel="stylesheet" href="/assets/css/gijgo.css">
+        <link rel="stylesheet" href="/assets/css/slicknav.css">
+        <link rel="stylesheet" href="/assets/css/animate.min.css">
+        <link rel="stylesheet" href="/assets/css/magnific-popup.css">
+        <link rel="stylesheet" href="/assets/css/fontawesome-all.min.css">
+        <link rel="stylesheet" href="/assets/css/themify-icons.css">
+        <link rel="stylesheet" href="/assets/css/themify-icons.css">
+        <link rel="stylesheet" href="/assets/css/slick.css">
+        <link rel="stylesheet" href="/assets/css/nice-select.css">
+        <link rel="stylesheet" href="/assets/css/style.css">
+        <link rel="stylesheet" href="/assets/css/responsive.css">
+        <!-- Language switcher script -->
+        <script src="/assets/js/language-switcher.js"></script>
+            <script>
+                window.currentLanguage = '{{ $language ?? "es" }}';
+            </script>
+   </head>
 
 <body data-language="{{ session('language') ?? 'es' }}">
     <!-- Preloader Start -->
