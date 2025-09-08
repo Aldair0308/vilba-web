fd8a9dc381883880f4af95bf473cd416e211431d
Aldair0308 <aldairm0308@gmail.com>
2025-09-07 23:00:51 -0600

    refactor(database): simplify mongodb connection configuration
    Use DSN string instead of separate host/port/username/password fields for cleaner configuration and better alignment with MongoDB driver's recommended connection approach

--- config/database.php
+++ config/database.php
@@ -33,11 +33,8 @@ return [
 
         'mongodb' => [
             'driver' => 'mongodb',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', 27017),
+            'dsn' => env('DB_URI', 'mongodb://127.0.0.1:27017'),
             'database' => env('DB_DATABASE', 'vilba-db'),
-            'username' => env('DB_USERNAME', ''),
-            'password' => env('DB_PASSWORD', ''),
             'options' => [
                 // here you can pass more settings to the Mongo Driver Manager
                 // see https://www.php.net/manual/en/mongodb-driver-manager.construct.php under "Uri Options" for a list of complete parameters that you can use
